49180b2d6faabd227cd96282adc187c45bb652e8
Cadimiel Pedro <98290326+Nidaime-Uchihacker@users.noreply.github.com>
2022-06-04 08:49:37 +0100

    Create special_effect.py
    Efeito especial em Python usando turtle. Apenas por diversão.

---
+++ special_effect.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+code = "Creted for Nidaime Pedro Uchihacker\n08:47 4/06/2022"
+speed(10)
+color('cyan')
+bgcolor('black')
+b = 150
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
